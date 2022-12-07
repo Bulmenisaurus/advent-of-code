9bdee11ee5c283728205a129e9020615bde302b2
alexey <alexeybulmeni@gmail.com>
2022-12-06 20:22:45 -0800

    day 6

---
+++ 2022/day06b/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut has_result_been_found = false;
+    include_str!("./input.txt")
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(14)
+        .enumerate()
+        .for_each(|(i, window)| {
+            if has_result_been_found {
+                return ();
+            }
+
+            for i in 0..window.len() - 1 {
+                for j in i + 1..window.len() {
+                    if window[i] == window[j] {
+                        return;
+                    }
+                }
+            }
+
+            println!("{}", i + 14);
+            has_result_been_found = true;
+        });
+}
